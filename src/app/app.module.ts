25d12d13e7deba08f5d65327fc9daa3d32c6318f
oraddyo <radu.g@outlook.com>
2018-05-11 19:22:48 +0300

    Scoreboard component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BoardComponent } from './board/board.component';
+import { ScoreboardComponent } from './scoreboard/scoreboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BoardComponent
+    BoardComponent,
+    ScoreboardComponent
   ],
   imports: [
     BrowserModule
