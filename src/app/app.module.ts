d71ca37ac1466e7edb855c2936ec6d5c683511ff
oraddyo <raddu14@yahoo.com>
2018-05-11 19:08:46 +0300

    Header and board components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BoardComponent } from './board/board.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule
